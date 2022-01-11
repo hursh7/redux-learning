e85ce75a4dfa017d81fca4f1cb6c01918ddaa962
junhp7 <junhp1994@gmail.com>
2022-01-11 14:46:59 +0900

    add reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import { createStore } from 'redux';
 
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const number = document.querySelector('#number');
+
+const countModifier = (state = 0) => {
+  // state initializing
+  // data를 바꾸고 modify하는 걸 책임진다.(in charge of)
+  console.log(state);
+  return state;
+};
+
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
